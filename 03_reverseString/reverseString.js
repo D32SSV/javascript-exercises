bbccd1a13e95c062bba74e9bab84d61c93544d9e
D32SSV <131158288+D32SSV@users.noreply.github.com>
2023-07-21 02:11:28 +0530

    solve exe 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let lengthOfString = string.length ;
+    let result = "" ;
+    for(let i = lengthOfString-1; i>=0; i--){
+        result += string.charAt(i) ;
+    }
 
+    return result ;
 };
 
 // Do not edit below this line

c9a64d13452394f63b5f8112215ae6a1ffcda4f1
D32SSV <131158288+D32SSV@users.noreply.github.com>
2023-07-21 02:04:52 +0530

    solve first two questions, understand test run is done in Terminal

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+let string = 'hey';
 
+const repeatString = function(string, number) {
+
+    if(number < 0) return "ERROR";
+    if(number == 0) return '';
+    let i = 0;
+    let result = string;
+    while(i<number-1){
+        result += string ;
+        i++;
+    }
+    return result ;
 };
 
 // Do not edit below this line

c863bebd9d30ae382f87806fe6803aa1e77d5783
D32SSV <131158288+D32SSV@users.noreply.github.com>
2023-07-21 21:18:24 +0530

    use filter method & includes method to solve

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) {
+    const result = array.filter((value) => {
+        return !args.includes(value)
+    })
+    return result;
 };
 
 // Do not edit below this line
